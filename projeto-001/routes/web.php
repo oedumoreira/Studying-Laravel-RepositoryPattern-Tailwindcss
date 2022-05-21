f9129d10610fed90c675868f6f8f40ac5eae51e3
Eduardo Moreira <edumoreirajj@gmail.com>
2022-05-21 13:06:22 -0300

    First Commit

---
+++ projeto-001/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\AdminProductController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/product', [ProductController::class, 'show']);
+
+
+
+//Admin
+Route::get('/admin/products', [AdminProductController::class, 'index']);
+Route::get('/admin/products/edit', [AdminProductController::class, 'edit']);
