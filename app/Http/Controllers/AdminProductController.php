951238724810a782b4d0bc6711cc7e1e5c95c6ce
Eduardo Moreira <edumoreirajj@gmail.com>
2022-07-17 00:13:25 -0300

    reorganization

---
+++ app/Http/Controllers/AdminProductController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Str;
+
+class AdminProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return view('admin.products', compact('products'));
+    }
+
+    //SHOW PAGE EDITION PRODUCT
+    public function edit(Product $product) 
+    {
+        return view('admin.product_edit', [
+            'product' => $product
+        ]);
+    }
+
+    //REQUEST UPDATE PRODUCT
+    public function update(Product $product, Request $request) 
+    {
+        $input = $request->validate([
+            'name' => 'string|required',
+            'price' => 'string|required',
+            'stock' => 'integer|nullable',
+            'cover' => 'file|nullable',
+            'description' => 'string|nullable'
+        ]);
+
+        $input['slug'] = Str::slug($input['name']);
+
+        if (!empty($input['cover'] && $input['cover']->isValid())) {
+            $file = $input['cover'];
+            $path = $file->store('public/products');
+            $input['cover'] = $path;
+        }
+        
+        $product->fill($input);
+        $product->save();
+
+        return Redirect::route('admin.products');
+
+        dd($input);
+
+    }
+
+    //SHOW PAGE CREATE PRODUCT
+    public function create()
+    {
+        return view('admin.product_create');
+    }
+
+    //REQUEST CREATE NEW PRODUCT
+    public function store(Request $request)
+    {
+        $input = $request->validate([
+            'name' => 'string|required',
+            'price' => 'string|required',
+            'stock' => 'integer|nullable',
+            'cover' => 'file|nullable',
+            'description' => 'string|nullable'
+        ]);
+        $input['slug'] = Str::slug($input['name']);
+
+        if (!empty($input['cover'] && $input['cover']->isValid())) {
+            $file = $input['cover'];
+            $path = $file->store('public/products');
+            $input['cover'] = $path;
+        }
+
+        Product::create($input);
+
+        return Redirect::route('admin.products');
+    }
+}
